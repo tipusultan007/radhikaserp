1cc451cc7e126f1def3b9c663ca9dc7336dc5348
Tipu Sultan <tipusultan.ctg@gmail.com>
2026-08-06 14:15:03 +0600

    Fix Steadfast Courier API base URL to portal.packzy.com

--- config/services.php
+++ config/services.php
@@ -42,7 +42,7 @@ return [
     ],
 
     'steadfast' => [
-        'url' => env('STEADFAST_BASE_URL', 'https://portal.steadfast.com.bd/api/v1'),
+        'url' => env('STEADFAST_BASE_URL', 'https://portal.packzy.com/api/v1'),
         'api_key' => env('STEADFAST_API_KEY'),
         'secret_key' => env('STEADFAST_SECRET_KEY'),
     ],
